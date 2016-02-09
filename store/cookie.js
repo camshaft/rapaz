b3c1d48da5e3e394c36e9c066e066c53938ce0f2
Brandon Bassett <bassett.brandon@gmail.com>
2016-02-09 14:49:34 -0700

    address hostnames with more than 3 parts

--- store/cookie.js
+++ store/cookie.js
@@ -17,10 +17,23 @@ CookieStore.prototype = {
   },
   set: function(key, value, ttl) {
     var date = (new Date(+new Date + ((typeof ttl === 'undefined' ? 63244800 : ttl) * 1000))).toUTCString();
-    var domainParts = location.host.split('.');
-    domainParts.shift();
-    var domain = '.' + domainParts.join('.');
-    document.cookie = key + '=' + value + '; path=/; expires=' + date + '; domain=' + domain;
+    var revHost = location.hostname.split('.').reverse();
+
+    // localhost
+    if(revHost.length === 1) {
+      document.cookie = key + '=' + value + '; path=/; expires=' + date + '; domain=' + location.hostname;
+    } else {
+      var domainParts = [];
+
+      // move backward through hostname, until cookie is successfully set. This addresses TLD's such as google.com.br
+      for(var i=0; i<revHost.length; i++){
+        if(CookieStore.prototype.get(key) == null || CookieStore.prototype.get(key) != value) {
+          domainParts.unshift(revHost[i]);
+          document.cookie = key + '=' + value + '; path=/; expires=' + date + '; domain=.' + domainparts.join('.');
+        }
+      }
+    }
   },
   isEnabled: require('../lib/browser/features/cookies')
 };
+
