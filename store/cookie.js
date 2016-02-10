f2ae0a8133fd9be09f46032b184272aab821ab4b
camshaft <bytheway.cameron@gmail.com>
2016-02-09 17:19:05 -0700

    set domain wildcard cookies

--- store/cookie.js
+++ store/cookie.js
@@ -1,7 +1,7 @@
 module.exports = CookieStore;
 
-function CookieStore() {
-
+function CookieStore(domain) {
+  this.domain = domain;
 }
 
 CookieStore.prototype = {
@@ -16,7 +16,29 @@ CookieStore.prototype = {
     }
   },
   set: function(key, value, ttl) {
-    document.cookie = key + '=' + value + '; path=/; expires=' + (new Date(+new Date + ((typeof ttl === 'undefined' ? 63244800 : ttl) * 1000))).toUTCString();
+    if (!this.domain) return this._fetchDomain(key, value, ttl);
+    this._set(key, value, ttl);
+  },
+  _fetchDomain: function(key, value, ttl) {
+    var hostname = location.hostname;
+    var parts = hostname.split('.').reverse();
+
+    if (parts.length === 1) {
+      this.domain = hostname;
+      return this._set(key, value, ttl);
+    }
+
+    for (var i = 2; i < parts.length; i++) {
+      this.domain = '.' + parts.slice(0, i).reverse().join('.');
+      this._set(key, value, ttl);
+      if (this.get(key) === value) return;
+    }
+    this.domain = hostname;
+    this._set(key, value, ttl);
+  },
+  _set: function(key, value, ttl) {
+    var date = (new Date(+new Date + ((typeof ttl === 'undefined' ? 63244800 : ttl) * 1000))).toUTCString();
+    document.cookie = key + '=' + value + '; path=/; expires=' + date + '; domain=' + this.domain;
   },
   isEnabled: require('../lib/browser/features/cookies')
 };
